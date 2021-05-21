c0bced8bee95c654083e91c0436a826bd85cec8b
Natanael Almeida <13645541+natanjalmeida@users.noreply.github.com>
2021-05-21 18:23:59 -0400

    RpboSinacom folder

---
+++ RoboSinacom/RoboSharp-dev/RoboSharp/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿namespace RoboSharp
+{
+    public static class ExtensionMethods
+    {
+        public static string CleanOptionInput(this string option)
+        {
+            // Get rid of forward slashes
+            option = option.Replace("/", "");
+            // Get rid of padding
+            option = option.Trim();
+
+            return option;
+        }
+    }
+}
